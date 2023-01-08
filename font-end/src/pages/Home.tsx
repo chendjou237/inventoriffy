bee146800aa8445b13b2dac523c965be6304c155
mbohbless <mbohblesspearl@gmail.com>
2023-01-08 17:19:02 +0100

    changing filed

--- font-end/src/pages/Home.tsx
+++ font-end/src/pages/Home.tsx
@@ -1,8 +1,10 @@
 import React, { useState } from "react";
 import { FeaturesCard, StatsComponent } from "../components";
+import { FEATURES } from "../shared";
 
 const Home = () => {
   const [navbar, setNavbar] = useState(false);
+  const [features,setFeatures] = useState(FEATURES);
   return (
     <>
       <nav className="w-full bg-base-300 shadow lg:py-5 ">
@@ -136,42 +138,12 @@ const Home = () => {
        
           Features of the application
         </h1>
-        <div className="flex flex-col md:flex-row  space-y-4 ">
-          <FeaturesCard
-            imageUrl="https://us.123rf.com/450wm/skypicsstudio/skypicsstudio1904/skypicsstudio190400064/skypicsstudio190400064.jpg?ver=6"
-            title="Wharehouse management"
-            desc="A warehouse management system (WMS) provides real-time visibility on a business’ inventory and operations"
-          />
-       
-          <FeaturesCard
-            imageUrl="https://www.pngitem.com/pimgs/m/333-3331732_intelligent-clipart-human-brain-business-intelligence-brain-hd.png"
-            title="Intelligent reporting and analytics"
-            desc="? Business Intelligence reporting is broadly defined as the process of using a BI tool to prepare and analyze data to find and share actionable insights"
-          />
-          
-          <FeaturesCard
-            imageUrl="https://thumbs.dreamstime.com/b/work-performance-schedule-staff-discipline-time-attendance-tracking-system-office-time-tracking-employee-time-management-155073624.jpg"
-            title="Stock Tracking"
-            desc="A tracking stock is a special equity offering issued by a parent company that tracks the financial performance of a particular segment or divisio"
-          />       
-        </div>
-        <div className="flex flex-col md:flex-row mt-10  mb-10 space-y-4">
-          <FeaturesCard
-            imageUrl="https://icon2.cleanpng.com/20180326/coq/kisspng-cartoon-graphic-design-office-5ab938d0114a77.2107562315220881440708.jpg"
-            title="Busibess to business"
-            desc="
-B2B e-commerce, short for business-to-business electronic commerce, is the sale of goods or services between businesses via an online sales portal. "
-          />
-          <FeaturesCard
-            imageUrl="https://corporate.payu.com/wp-content/uploads/2022/02/payment-gateway-generic_990x640.png"
-            title="Payment Gateway"
-            desc="A payment gateway is a merchant service provided by an e-commerce application service provider that authorizes credit card or direct payments processing for e-businesses, online retailers, bricks and clicks, or traditional brick and mortar. "
-          />
-          <FeaturesCard
-            imageUrl="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRTXLedG0u3xD-jpk95kJLZFSnXHSgvujiUTPa64_oe&s"
-            title="Purchase and Sales Order"
-            desc="Purchase orders are used by buyers to initiate the purchasing process with a supplier. "
-          />
+        <div className="flex flex-col md:grid grid-cols-3 lg:grid-cols-4 gap-4 ">
+          {features.map((feature)=> <FeaturesCard
+            imageUrl={feature.imageUrl}
+            title={feature.title}
+            desc={feature.title}/>)}
+     
         </div>
       </div>
     </>
