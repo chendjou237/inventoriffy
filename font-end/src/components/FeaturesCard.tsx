c05eb46948e5dd73aeae4cf1ac81e2994909ffeb
mbohbless <mbohblesspearl@gmail.com>
2023-01-08 17:19:02 +0100

    changing filed

--- font-end/src/components/FeaturesCard.tsx
+++ font-end/src/components/FeaturesCard.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const FeaturesCard = (props:any) => {
   return (
-    <div className="w-full md:w-1/2 lg:w-1/3 mx-5 ">
+    <div className="col-auto  ">
             <div className="card bg-base-300 shadow-xl">
               <figure className="px-10 pt-10">
                 <img
