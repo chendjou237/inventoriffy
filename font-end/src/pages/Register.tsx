2194a2a3bdb1c737dbac9469b5f76537770f47bf
mbohbless <mbohblesspearl@gmail.com>
2023-01-08 20:06:17 +0100

    register page

--- font-end/src/pages/Register.tsx
+++ font-end/src/pages/Register.tsx
@@ -5,12 +5,24 @@ const Register = () => {
   const [email, setEmail] = useState("");
   const [name, setName] = useState("");
   const [password, setPassword] = useState("");
+  const [busineddName, setBusinessName] = useState("");
+  const [businessEmail, setBusinessEmail] = useState("");
+  const [businessContact, setBusinessContact] = useState("");
+  const [businessLogo, setBusinessLogo] = useState("");
+  const [ownerId, setOwnerId] = useState("");
   const [phoneNumber, setNumber] = useState("");
+
   const [isLastScreen, setIsLastScreen] = useState(false);
-  const handleSubmit=(e:React.FormEvent)=>{
+  const handleNavigate=(e:React.FormEvent)=>{
     e.preventDefault()
     setIsLastScreen(true);
   }
+  const handleSubmit=(e:React.FormEvent)=>{
+    e.preventDefault()
+    
+  }
+
+
   return (
     <>
     <div className="container pt-5">
@@ -18,11 +30,11 @@ const Register = () => {
 <BsFillArrowLeftCircleFill className='text-5xl ml-5' onClick={()=>setIsLastScreen(false)}/>
 <h4 className="block text-2xl ml-4 mt-2" > Back to personal Information </h4>  
 </div>}
-{ !isLastScreen && 
+{ !isLastScreen ?
   <div className="hero h-64 ">
 
- <form onSubmit={handleSubmit}>
-  <div className="w-full max-w-lg md:shadow-2xl bg-base-300 mt-10 lg:mt-10" >
+ <form onSubmit={handleNavigate}>
+  <div className="w-full max-w-lg md:shadow-2xl bg-base-300 mt-10 lg:mt-20" >
     <div className=" p-20">
       <h3 className=' text-2xl text-center'>
         Enter your personal details to create account
@@ -63,7 +75,48 @@ const Register = () => {
     </div>
   </div>
   </form>
-  </div>}
+  </div>:<div className="hero h-64 ">
+
+<form action='post' onSubmit={handleSubmit}>
+ <div className="w-full max-w-lg md:shadow-2xl bg-base-300 mt-10 lg:mt-10" >
+   <div className=" p-20">
+     <h3 className=' text-2xl text-center'>
+     Enter your business details to create account and handle your business services
+     </h3>
+      <div className="form-control">
+       <label className="label">
+         <span className="label-text">Business Name</span>
+       </label>
+       <input type="text" placeholder="Business name" className="input input-bordered" value={busineddName}  onChange={(e) => setBusinessName(e.target.value)} required/>
+       
+     </div>
+     <div className="form-control">
+       <label className="label">
+         <span className="label-text">Business Contact</span>
+       </label>
+       <input type="tel" placeholder="Contact" className="input input-bordered" value={businessContact}  onChange={(e) => setBusinessContact(e.target.value)} required/>
+       
+     </div>
+     <div className="form-control w-full">
+       <label className="label">
+         <span className="label-text">Business Email</span>
+       </label>
+       <input type="email"  placeholder="Email" value={businessEmail} onChange={(e) => setBusinessEmail(e.target.value)} className="input input-bordered" required/>
+     </div>
+      <div className="form-control">
+        <label className="label">
+          <span className="label-text">Business Logo</span>
+        </label>
+        <input type="file" value={businessLogo} onChange={(e) => setBusinessLogo(e.target.value)} className="input input-bordered form-input mt-2"
+      accept="image/*"required />
+      </div>
+     <div className="form-control mt-6">
+       <button className="btn btn-primary">Continue</button>
+     </div>
+   </div>
+ </div>
+ </form>
+ </div>}
 </div>
 
     </>
@@ -71,3 +124,4 @@ const Register = () => {
 }
 
 export default Register
+
