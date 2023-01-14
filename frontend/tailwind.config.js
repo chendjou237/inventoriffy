aa2cf17142e893b97b3aece2b1b5338ff416ec55
chendjou237 <chenxhenor@gmail.com>
2023-01-14 22:29:40 +0100

    name🐞 fix:

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,23 +1,49 @@
-/** @type {import('tailwindcss').Config} */
-const colors = require('tailwindcss/colors')
-
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
-  theme: {
-    colors: {
-      transparent: 'transparent',
-      current: 'currentColor',
-      black: colors.black,
-      white: colors.white,
-      gray: colors.slate,
-      green: colors.emerald,
-      purple: colors.violet,
-      yellow: colors.amber,
-      pink: colors.fuchsia,
+    content: ['./src/**/*.{js,jsx,ts,tsx}'],
+    darkMode: 'class',
+    theme: {
+  
+      fontFamily: {
+        display: ['Open Sans', 'sans-serif'],
+        body: ['Open Sans', 'sans-serif'],
+      },
+      extend: {
+        fontSize: {
+          14: '14px',
+        },
+        backgroundColor: {
+          'main-bg': '#FAFBFB',
+          'main-dark-bg': '#20232A',
+          'secondary-dark-bg': '#33373E',
+          'light-gray': '#F7F7F7',
+          'half-transparent': 'rgba(0, 0, 0, 0.5)',
+        },
+        borderWidth: {
+          1: '1px',
+        },
+        borderColor: {
+          color: 'rgba(0, 0, 0, 0.1)',
+        },
+        width: {
+          400: '400px',
+          760: '760px',
+          780: '780px',
+          800: '800px',
+          1000: '1000px',
+          1200: '1200px',
+          1400: '1400px',
+        },
+        height: {
+          80: '80px',
+        },
+        minHeight: {
+          590: '590px',
+        },
+        backgroundImage: {
+          'hero-pattern':
+            "url('https://i.ibb.co/FzvV49b/welcome-bg-svg-3.png')",
+        },
+      },
     },
-  },
-  plugins: [require("daisyui")],
-
-}
+    plugins: [require("daisyui")],
+  };
