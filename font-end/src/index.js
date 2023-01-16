ad0011016992c58611cf96837cdef948033f84c6
mbohbless <mbohblesspearl@gmail.com>
2023-01-16 12:39:21 +0100

    order fetch

--- font-end/src/index.js
+++ font-end/src/index.js
@@ -2,13 +2,31 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, createBrowserRouter, RouterProvider } from "react-router-dom";
 import { ContextProvider } from "./context/ContextProvider";
+import { Home, Login, Register } from "./pages";
 
+const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <Home />,
+    },
+    {
+      path: "/login",
+      element: <Login />,
+    },
+    {
+      path: "/register",
+      element: <Register />,
+    }
+  ]
+)
 createRoot(document.getElementById("root")).render(
   <ContextProvider>
-    <BrowserRouter>
+    {/* <RouterProvider router={router}/> */}
+    {/* <BrowserRouter> */}
       <App />
-    </BrowserRouter>
+    {/* </BrowserRouter> */}
   </ContextProvider>
 );
