a43df5e8fa38204110939f7cc159902faf9fe443
mbohbless <mbohblesspearl@gmail.com>
2023-01-18 22:36:38 +0100

    80% order screen

--- font-end/src/pages/OrderCreate.jsx
+++ font-end/src/pages/OrderCreate.jsx
@@ -1,10 +1,18 @@
 import React, { useEffect, useState } from 'react'
 import { dummmyProduct, paymentMethods } from '../data/dummy'
-
+import { useStateContext } from '../context/ContextProvider'
+import { baseUrl } from '../data/base_url'
+import { useNavigate } from "react-router-dom"
 const OrderCreate = () => {
+    const {
+        setOrderData,
+        ordersError,
+        setOrdersError,
+    } = useStateContext();
     const [maxQuantity, setmaxQuantity] = useState(0)
     const [paymentMethod, setPaymentMethod] = useState("")
-    const [processedTransaction, setProcessedTransaction] = useState(null)
+
+    const [showModal, setShowModal] = useState(true);
     useEffect(() => {
         var e = document.getElementById("product");
         var maxQ = parseInt(e.value)
@@ -113,12 +121,12 @@ const OrderCreate = () => {
                     })
                     .catch((error) => {
                         console.log("post comments", error.message);
-                        alert("Your comment could not be posted\nError: " + error.message);
+                        setShowModal(true)
                     });
             })
             .catch((error) => {
                 console.log("post comments", error.message);
-                alert("Your comment could not be posted\nError: " + error.message);
+                setShowModal(true)
             });
 
     }
@@ -279,6 +287,61 @@ const OrderCreate = () => {
                 </div>
             </div>
 
+
+            {/* <button data-modal-target="defaultModal" data-modal-toggle="defaultModal" class="block text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" type="button">
+                Toggle modal
+            </button> */}
+            {showModal && (
+                <>
+                    <div
+                        className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none"
+                    >
+                        <div className="relative w-auto my-6 mx-auto max-w-3xl">
+                            {/*content*/}
+                            <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
+                                {/*header*/}
+                                <div className="flex items-start justify-between p-5 border-b border-solid border-slate-200 rounded-t">
+                                    <h3 className="text-3xl font-semibold">
+                                        Modal Title
+                                    </h3>
+                                    <button
+                                        className="p-1 ml-auto bg-transparent border-0 text-black opacity-5 float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
+                                        onClick={() => setShowModal(false)}
+                                    >
+                                        <span className="bg-transparent text-black opacity-5 h-6 w-6 text-2xl block outline-none focus:outline-none">
+                                            ×
+                                        </span>
+                                    </button>
+                                </div>
+                                {/*body*/}
+                                <div className="relative p-6 flex-auto">
+                                    <p className="my-4 text-slate-500 text-lg leading-relaxed">
+                                        There was an error performing the transaction. Please try again later
+                                    </p>
+                                </div>
+                                {/*footer*/}
+                                <div className="flex items-center justify-center p-6 border-t border-solid border-slate-200 rounded-b">
+                                    {/* <button
+                                        className="text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                                        type="button"
+                                        onClick={() => setShowModal(false)}
+                                    >
+                                        Close
+                                    </button> */}
+                                    <button
+                                        className="bg-red-500 text-white active:bg-emerald-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                                        type="button"
+                                        onClick={() => setShowModal(false)}
+                                    >
+                                        Try again later
+                                    </button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
+                </>
+            )}
 
         </>
     )
